1564299719e666d59eefe453159086b3b3808ff3
Vaibhav <vaibhav.katkar2302@gmail.com>
2024-07-20 00:40:19 +0530

    "Added code for angular from V1"

---
+++ angular-form-project/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'angular-form-project';
+  angularFormTest : FormGroup | any;
+
+  ngOnInit(): void {
+      this.angularFormTest = new FormGroup({
+        name : new FormControl('', Validators.required),
+        email : new FormControl('',Validators.email),
+        phone : new FormControl('',Validators.minLength(10))
+      })
+  }
+}
