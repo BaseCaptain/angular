29ea5005b44620650735bea3561a36b48170c7f2
Vaibhav <vaibhav.katkar2302@gmail.com>
2024-07-21 19:22:53 +0530

    update TODO

--- angular-form-project/src/app/app.component.ts
+++ angular-form-project/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   title = 'angular-form-project';
   angularFormTest : FormGroup | any;
+  date = new Date();
 
   ngOnInit(): void {
       this.angularFormTest = new FormGroup({
@@ -19,8 +20,31 @@ export class AppComponent implements OnInit {
   }
 
   // question in angular
-  // 1. Pipes in angular
-  // 2. Communication between components
+  // 1. Pipes in angular --- Done
+  // 2. Communication between components --- multiple ways
+      // query params, input output
   // 3. Authguard
+      // to secure your url
   // 4. Interceptor
+  // 5. Lazy loading ---- Done
+
+  // TODO
+  // need to done
+  // query params
+  // path varidable
+  // diff between normal and standalone application in angular
+
+
+
+  // Agile Methodlogy
+    // tool --- JIRA
+    // Sprint ? -- > 2 week
+    // backlog refinement ---> queries regarding task get resolved with Product owner
+    // Sprint planning ----> task you are going to pickup in next sprint it is going to discuss
+    // Sprint review ---> after your 2 week sprint got over, you will give review of your task to Product owner
+
+    // benefits
+    // tujhe doubt resolve hoote before starting your task
+    // tujhe dsicussion hun janar tu kuthe task gheshil
+    // if review madhe owner ni kahi changes sangitle tr te next sprint madhe cover hun janar
 }
